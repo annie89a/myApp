eb7ab90d6a6d85825a906df15321209b69264355
anisha <anisha89a@gmail.com>
2024-11-22 13:04:01 +0800

    comment modification

--- src/main/java/com/telusko/myApp/Dev.java
+++ src/main/java/com/telusko/myApp/Dev.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Component;
 public class  Dev {
 
   @Autowired // field injection
-  @Qualifier("laptop") //here, name of the class , but lowercase
+  @Qualifier("laptop") //this should be the name of the class , but lowercase
     private Computer comp;
 
 //   public Dev(Laptop laptop) //constructor injection, @Autowired is not required here, its optional
